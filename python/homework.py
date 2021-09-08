c5beea5414e10895f4e8d8dfa24d9b04305a0174
CHERRY <lp1969@mail.ru>
2021-09-08 18:02:48 +0300

    homework1

---
+++ python/homework.py
@@ -0,0 +1,54 @@
+
+# Задание
+name = "Liliya"   # str
+print(name)
+age = 45  # int
+print(age)
+weight = 47.5  # float
+print(weight)
+title = ["name", "age", "weight"]  # list
+print(title)
+comp_lex = 2+3j  # complex
+print(comp_lex)
+Bytes = bytes([5, 10, 100])  # bytes
+print(Bytes)
+cort = tuple("Home work")  # tuple (кортеж)
+print(cort)
+Set = set("hello")   # set (случайный порядок) может изменяться
+print(Set)
+F_set = frozenset ("Привет")   # frozenset ( нельзя изменить )
+print(F_set)
+Dict = dict(first=1, second=2)   # dict (словарь)
+print(Dict)
+
+# Задание
+
+name = "Liliya"
+surname = "Cherry"
+user_name = (name + surname)
+print (user_name)
+
+# Задание
+
+first_int = 20
+second_int = 30
+result = (first_int + second_int)
+print (result)
+result = (first_int * second_int)
+print (result)
+result = (first_int / second_int)
+print (result)
+result = (first_int / second_int)
+print(round(result))
+result = (first_int % second_int)
+print(result)
+
+
+
+
+
+
+
+
+
+
